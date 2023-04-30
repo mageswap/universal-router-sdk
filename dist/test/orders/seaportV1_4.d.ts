f03be4e1f0be07aa2a65cf0416e7ed5c52b5cece
~ . ~ <81006672+lilesper@users.noreply.github.com>
2023-04-30 17:47:14 -0400

    we do a lil distributin

---
+++ dist/test/orders/seaportV1_4.d.ts
@@ -0,0 +1,3 @@
+import { SeaportData } from '../../src/entities/protocols/seaport';
+export declare const seaportV1_4DataETH: SeaportData;
+export declare const seaportV1_4DataETHRecent: SeaportData;
---
+++ dist/utils/getNativeCurrencyValue.d.ts
@@ -0,0 +1,2 @@
+import { Currency, CurrencyAmount } from '@mageswap/sdk-core';
+export declare function getNativeCurrencyValue(currencyValues: CurrencyAmount<Currency>[]): CurrencyAmount<Currency>;

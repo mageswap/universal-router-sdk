8abad20f734d0073bbce29604e2a27fdf25ae9f5
~ . ~ <81006672+lilesper@users.noreply.github.com>
2023-05-19 12:49:45 -0400

    ~

--- dist/utils/constants.d.ts
+++ dist/utils/constants.d.ts
@@ -1,7 +1,7 @@
 import { BigNumber } from 'ethers';
 export declare const UNIVERSAL_ROUTER_ADDRESS: (chainId: number) => string;
 export declare const WETH_ADDRESS: (chainId: number) => string;
-export declare const PERMIT2_ADDRESS = "0x000000000022D473030F116dDEE9F6B43aC78BA3";
+export declare const PERMIT2_ADDRESS = "0xe820edC3aa5933966081850861a5f79517f9A72a";
 export declare const CONTRACT_BALANCE: BigNumber;
 export declare const ETH_ADDRESS = "0x0000000000000000000000000000000000000000";
 export declare const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
